dbbd14a3ad98f26df5f17d431bdc4c148a36c253
st.kvvignesh@gmail.com <vigneshautomation76@gmail.com>
2024-02-11 21:08:41 +0530

    chnage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React, { useState , useEffect} from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './loginpage';
-import Orders from './orders';
-import Product from './products';
-import Users from './users';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./loginpage";
+import Orders from "./orders";
+import Product from "./products";
+import Users from "./users";
 
 function App() {
-
-
   return (
-<Router>
-    <Routes>
-    <Route exact path="/" element={<Login />} />
-      <Route exact path="/orders" element={<Orders />} />
-      <Route exact path="/products" element={<Product />} />
-      <Route exact path="/users" element={<Users />} />
-    </Routes>
-  </Router>
-
-
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/orders" element={<Orders />} />
+        <Route exact path="/products" element={<Product />} />
+        <Route exact path="/users" element={<Users />} />
+      </Routes>
+    </Router>
   );
-};
-
-
-
+}
 
 export default App;
-
